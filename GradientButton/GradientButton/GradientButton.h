96b08faa3fe106f0bc601553e21f969a3bdfe94a
suhitpatil <suhit.patil@gmail.com>
2019-08-10 16:10:32 +0530

    initial commit: added GradientButton subclass to source folder

---
+++ GradientButton/GradientButton/GradientButton.h
@@ -0,0 +1,19 @@
+//
+//  GradientButton.h
+//  GradientButton
+//
+//  Created by Suhit Patil on 10/08/19.
+//  Copyright © 2019 Suhit Patil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GradientButton.
+FOUNDATION_EXPORT double GradientButtonVersionNumber;
+
+//! Project version string for GradientButton.
+FOUNDATION_EXPORT const unsigned char GradientButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GradientButton/PublicHeader.h>
+
+
---
+++ GradientButton/GradientButton/Source/GradientButton.swift
@@ -0,0 +1,95 @@
+//
+//  GradientButton.swift
+//  GradientButton
+//
+//  Created by Suhit Patil on 10/08/19.
+//  Copyright © 2019 Suhit Patil. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+open class GradientButton: UIButton {
+    
+    @IBInspectable
+    open var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable
+    open var startColor: UIColor = UIColor.red {
+        didSet {
+            gradientLayer.colors = [ startColor.cgColor, endColor.cgColor ]
+        }
+    }
+    
+    @IBInspectable
+    open var endColor: UIColor = UIColor.orange {
+        didSet {
+            gradientLayer.colors = [ startColor.cgColor, endColor.cgColor ]
+        }
+    }
+    
+    @IBInspectable
+    open var startPoint: CGPoint = CGPoint(x: 0.0, y: 0.5) {
+        didSet {
+            gradientLayer.startPoint = startPoint
+        }
+    }
+    
+    @IBInspectable
+    open var endPoint: CGPoint = CGPoint(x: 1.0, y: 0.5) {
+        didSet {
+            gradientLayer.endPoint = endPoint
+        }
+    }
+    
+    private lazy var gradientLayer: CAGradientLayer = {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.needsDisplayOnBoundsChange = true
+        gradient.colors = [startColor, endColor].map { $0.cgColor }
+        gradient.startPoint = startPoint
+        gradient.endPoint = endPoint
+        gradient.locations = [0.0, 1.0]
+        return gradient
+    }()
+    
+    //MARK: Init
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    override public func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setup()
+    }
+    
+    //MARK: setup
+    private func setup() {
+        layer.insertSublayer(gradientLayer, at: 0)
+        setCornerRadius()
+    }
+    
+    private func setCornerRadius() {
+        layer.cornerRadius = cornerRadius
+        layer.masksToBounds = cornerRadius > 0
+        if #available(iOS 13.0, *) {
+            layer.cornerCurve = .continuous
+        }
+    }
+    
+    open override func layoutSublayers(of layer: CALayer) {
+        super.layoutSublayers(of: layer)
+        gradientLayer.frame = bounds
+    }
+}
+
